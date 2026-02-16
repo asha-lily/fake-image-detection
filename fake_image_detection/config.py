b70a0671ae1f179cf42da6b050e9915ddf26cc5a
asha-lily <ashalilypatel@gmail.com>
2026-02-16 22:02:36 +0000

    Update image size and add model params to config.

--- fake_image_detection/config.py
+++ fake_image_detection/config.py
@@ -8,7 +8,7 @@ class Config:
     # dataset
     data_root_dir: Path = Path("/Users/ashapatel/Documents/projects/fake-image-detection/data")
     batch_size: int = 64
-    target_image_size: int = (768, 768)
+    target_image_size: int = (224, 224)
     prob_horizontal_flip: float = 0.25
     prob_jpeg_compress: float = 0.5
     prob_blur: float = 0.05
@@ -21,3 +21,7 @@ class Config:
     # consider making this configurable
 
     pretrained_model_name: str = "openai/clip-vit-base-patch16"
+
+    # model
+    embedding_size: int = 768
+    num_classes: int = 2
