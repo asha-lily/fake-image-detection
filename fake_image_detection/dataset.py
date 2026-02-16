2102837553129fab1e61291b4ea8f5879283bcfa
asha-lily <ashalilypatel@gmail.com>
2026-02-16 22:03:15 +0000

    Basic model training in notebook.

--- fake_image_detection/dataset.py
+++ fake_image_detection/dataset.py
@@ -1,6 +1,7 @@
 import os
 import logging
 from pathlib import Path
+from PIL import Image
 from transformers import CLIPProcessor
 import torchvision.transforms as transforms
 from fake_image_detection.config import Config
